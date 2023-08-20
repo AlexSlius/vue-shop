cd3d937501fe99d8652565a8eac720265be67785
Слюсарчук Олександр О <site.fron.end@gmail.com>
2023-08-20 18:45:44 +0300

    edit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
-import './assets/styles/jquery.fancybox.min.css'
-import './assets/styles/nouislider.min.css'
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './assets/styles/main.css'
-
-import './assets/js/libs/jquery/jquery.min.js'
-import './assets/js/libs/jquery.fancybox.min.js'
-import './assets/js/libs/nouislider.min.js'
-import './assets/js/libs/slick.min.js'
-import './assets/js/main'
 
 import directiveVMax from "@/directives/vMax";
 import directiveInputPrice from "@/directives/inputPrice";
